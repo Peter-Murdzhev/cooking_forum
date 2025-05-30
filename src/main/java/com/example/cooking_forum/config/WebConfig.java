beae38b643843b7e6c85a5316bf2041437f93b4a
Peter <murdzhevpetar@gmail.com>
2025-05-30 23:09:43 +0300

    updated cross origin configuration

--- src/main/java/com/example/cooking_forum/config/WebConfig.java
+++ src/main/java/com/example/cooking_forum/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer{
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500", "http://localhost:8080")
+                        .allowedOrigins("http://localhost:5500")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
