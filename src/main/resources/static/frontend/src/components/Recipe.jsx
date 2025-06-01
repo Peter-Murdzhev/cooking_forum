7b07a7093b7b447aa51d42fcc1e27c7346e49de2
Peter <murdzhevpetar@gmail.com>
2025-06-01 23:54:26 +0300

    fixed looks in mobile mode

--- src/main/resources/static/frontend/src/components/Recipe.jsx
+++ src/main/resources/static/frontend/src/components/Recipe.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Recipe = ({ recipe }) => {
 	const [imagePath, setImagePath] = useState("/images/recipes_pics/default_image.jpg")
+	const [description, setDescription] = useState("");
 
 	useEffect(() =>{
 		if(recipe.imageSource){
 			setImagePath(recipe.imageSource);
 		}
 	},[recipe.imageSource])
+
+	useEffect(() =>{
+		const renderDescription = () =>{
+		if(recipe.description.length > 60){
+			setDescription(`${recipe.description.substring(0, 60)}...`);
+		}else{
+			setDescription(recipe.description);
+		}
+	}
+
+	renderDescription();
+	},[recipe.description])
+	
 	
   return (
-	
 	<li className='recipe'>
 		<Link to={`/recipe/${recipe.id}`} >
 			<ul>
@@ -19,7 +32,7 @@ const Recipe = ({ recipe }) => {
 				<li style={{textAlign:"center", fontSize: "20px"}}><strong>
 					 {recipe.name}</strong></li>
 				<li style={{maxWidth:"200px", maxHeight:"200px", overflow:"hidden"}}>Description:<br />
-			 		{recipe.description} </li>
+			 		{description} </li>
 				<li style={{fontSize:"12px", textAlign: "right"}}>
 			 		{recipe.publishedDateTime} </li>
 			</ul>
